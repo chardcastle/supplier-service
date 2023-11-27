dd6577e7dc1315ff4a0cbaddc0f134e4e31c5afa
Chris Hardcastle <me@chrishardcastle.co.uk>
2023-11-27 22:44:26 +0000

    Removed abandoned dev

--- cypress/e2e/main.cy.js
+++
@@ -1,5 +0,0 @@
-describe('My main test', () => {
-    it('should test a file!', () => {
-        expect(true).to.equal(true)
-    })
-})
--- cypress/e2e/spec.cy.js
+++
@@ -1,5 +0,0 @@
-describe('My first spec test', () => {
-    it('Does not do much!', () => {
-        expect(true).to.equal(true)
-    })
-})
